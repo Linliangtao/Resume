4ec8fda763dcfec596e0275a04c25bd6528d3eb9
linliangtao <1428129601@qq.com>
2018-09-20 00:00:18 +0800

    存入数据库

--- js/message.js
+++ js/message.js
@@ -8,6 +8,24 @@ AV.init({
 });
 console.log("运行成功")
 
+let myForm = document.querySelector('#postMessageForm')
+
+myForm.addEventListener('submit',function(e){
+   e.preventDefault() //阻止页面刷新
+   let conctent = myForm.querySelector('input[name=content]').value
+   let Message = AV.Object.extend('Message');
+   var message = new Message();
+   message.save({
+    'content':conctent
+  }).then(function(object) {
+    alert('存入成功!');
+    console.log(Object);
+  })
+})
+
+
+
+/*
 //验证
 //创建TestObject 表
 var TestObject = AV.Object.extend('message');
@@ -20,3 +38,4 @@ testObject.save({
 }).then(function(object) {
   alert('LeanCloud 成功！');
 })
+*/
