b1a5d0c5a20c7cc67f6627f945202f84d69fd067
linliangtao <1428129601@qq.com>
2018-09-19 23:17:17 +0800

    测试数据库

--- js/message.js
+++ js/message.js
@@ -1,3 +1,4 @@
+//初始化
 var APP_ID = 'R7ODJzbG8zWSMala1WfmXWM3-gzGzoHsz';
 var APP_KEY = 'Umuc0mzenwPFCfrQnWiNNOhG';
 
@@ -6,3 +7,12 @@ AV.init({
   appKey: APP_KEY
 });
 console.log("运行成功")
+
+//验证
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud 成功！');
+})
