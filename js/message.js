14399f0b8416509bebf01c01e84a23e606a8b953
linliangtao <1428129601@qq.com>
2018-09-20 23:47:25 +0800

    添加MVC 的model

--- js/message.js
+++ js/message.js
@@ -1,62 +1,79 @@
 !function(){
-  //初始化
-  var view = document.querySelector('section.message')
-
-  var controller = {
-    view:null,
-    messageList:null,
-    init:function(view){
-      this.view = view
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('form')
-      this.initAV()
-      this.loadMessages()
-      this.bindEvnets()
-    },
-    initAV:function(){
+  var model = {
+    // 获取数据
+    init:function(){
       var APP_ID = 'R7ODJzbG8zWSMala1WfmXWM3-gzGzoHsz';
       var APP_KEY = 'Umuc0mzenwPFCfrQnWiNNOhG';
       AV.init({ appId: APP_ID,appKey: APP_KEY });
     },
-    loadMessages:function(){
+    fetch: function(){ 
       var query = new AV.Query('Message');
-      query.find()
-        .then(
-          (messages) =>{
-            let array = messages.map((item) => item.attributes)
-            array.forEach ((item)=> {
-              let li = document.createElement('li')
-              li.innerText = `${item.name}: ${item.content}`
-              this.messageList.appendChild(li)
-            })
-          }
-        )
+      return query.find() // Promise 对象
     },
-    bindEvnets:function(){   
-        this.form.addEventListener('submit',function(e){
-        e.preventDefault() //阻止页面刷新
-        this.saveMessage()
+    // 创建数据
+    save: function(name, content){
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      return message.save({  // Promise 对象
+        'name': name,
+        'content': content
       })
-    },    
-    saveMessage:function(){
-        let myForm = this.form
-        let content = myForm.querySelector('input[name=content]').value
-        let name = myForm.querySelector('input[name=name]').value   
-        let Message = AV.Object.extend('Message');
-        var message = new Message();
-        message.save({
-          'name':name,
-          'content':content
-        }).then(function(object) {
-          let li = document.createElement('li')
-          li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-          let messageList = document.querySelector('#messageList')
-          myForm.querySelector('input[name=content]').value = ''
-          messageList.appendChild(li)
-        })     
     }
   }
-  controller.init(view)
+
+  var view = document.querySelector('section.message')
+
+
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function(view, model){
+      this.view = view
+      this.model = model
+
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    loadMessages: function(){
+      this.model.fetch().then(
+        (messages) => {
+          let array = messages.map((item)=> item.attributes )
+          array.forEach((item)=>{
+            let li = document.createElement('li')
+            li.innerText = `${item.name}: ${item.content}`
+            this.messageList.appendChild(li)
+          })
+        } 
+      )
+    },
+    bindEvents: function(){
+      this.form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        this.saveMessage()
+      })
+    },
+    saveMessage: function(){
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      this.model.save(name, content).then(function(object) {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+        console.log(object)
+      })
+    }
+
+  }
+
+  controller.init(view, model)
+
 
 }.call()
 
