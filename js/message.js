637e20adb43259917646062ec9c174576b7b011c
linliangtao <1428129601@qq.com>
2018-09-27 22:57:54 +0800

    封装Model

--- js/message.js
+++ js/message.js
@@ -1,27 +1,7 @@
 //留言板功能
 
 !function(){
-  var model = {
-    // 获取数据
-    init:function(){
-      var APP_ID = 'R7ODJzbG8zWSMala1WfmXWM3-gzGzoHsz';
-      var APP_KEY = 'Umuc0mzenwPFCfrQnWiNNOhG';
-      AV.init({ appId: APP_ID,appKey: APP_KEY });
-    },
-    fetch: function(){ 
-      var query = new AV.Query('Message');
-      return query.find() // Promise 对象
-    },
-    // 创建数据
-    save: function(name, content){
-      var Message = AV.Object.extend('Message');
-      var message = new Message();
-      return message.save({  // Promise 对象
-        'name': name,
-        'content': content
-      })
-    }
-  }
+  var model = Model({resourceName:'Message'})
 
   var view = View('section.message')
 
@@ -62,7 +42,9 @@
       let myForm = this.form
       let content = myForm.querySelector('input[name=content]').value
       let name = myForm.querySelector('input[name=name]').value
-      this.model.save(name, content).then(function(object) {
+      this.model.save({
+        'name':name, 'content':content
+      }).then(function(object) {
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name}: ${object.attributes.content}`
         let messageList = document.querySelector('#messageList')
