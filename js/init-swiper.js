bf5ad19c25cb1e54995f2b001fa579ce4fc6bf4c
linliangtao <1428129601@qq.com>
2018-12-03 22:52:23 +0800

    优化一下

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,23 +1,53 @@
-//初始化轮播
-
-!function(){
+!function () {
   var view = View('#mySlides')
   var controller = {
-    view: null, 
-    swiper: null,
-    swiperOptions: { loop: true, pagination: { el: '.swiper-pagination', }, navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', }, },
-    init: function(view){
-      this.view = view
-      this.initSwiper()
-    },
-    initSwiper: function(){
-      this.swiper = new Swiper (
-        this.view.querySelector('.swiper-container'), 
-        this.swiperOptions
-      )
-    }
-  }
+      view: null,
+      swiper: null,
+      swiperOptions: {
+          autoplay: true,
+          touchRatio: 1,
+          loop: true,
+          slidesPerView: 5,
+          effect: 'coverflow',
+          grabCursor: true,
+          centeredSlides: true,
+          navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+          },
+          slidesPerView: 'auto',
+          coverflow: {
+              rotate: 0,// 旋转的角度
+              stretch: 100,// 拉伸   图片间左右的间距和密集度
+              depth: 50,// 深度   切换图片间上下的间距和密集度
+              modifier: 3,// 修正值 该值越大前面的效果越明显
+              slideShadows: false// 页面阴影效果
+          },
+      },
 
-  controller.init(view)
+          bindEvents: () => {
 
-}.call()    
+
+          },
+
+
+          init: function (view) {
+              this.view = view
+              this.initSwiper()
+              this.bindEvents()
+
+          },
+          initSwiper: function () {
+              this.swiper = new Swiper(
+                  this.view.querySelector('.swiper-container'),
+                  this.swiperOptions
+              )
+
+
+          }
+      }
+
+      controller.init(view)
+
+}.
+  call()
--- js/message.js
+++ js/message.js
@@ -1,60 +1,56 @@
-//留言板功能
+!function () {
+    var model = Model({resourceName: 'Message'})
+    var view = View('section.message')
 
-!function(){
-  var model = Model({resourceName:'Message'})
+    var controller = Controller({
 
-  var view = View('section.message')
 
-  var controller = Controller({
-    messageList: null,
-    form: null,
-    init: function(view,controller){
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('form')
-      this.loadMessages()
-    },
-  
-    loadMessages: function(){
-      this.model.fetch().then(
-        (messages) => {
-          let array = messages.map((item)=> item.attributes )
-          array.forEach((item)=>{
-            let li = document.createElement('li')
-            li.innerText = `${item.name}: ${item.content}`
-            this.messageList.appendChild(li)
-          })
-        } 
-      )
-    },
-    bindEvents: function(){
-      this.form.addEventListener('submit', (e)=>{
-        e.preventDefault()
-        this.saveMessage()
-      })
-    },
-    saveMessage: function(){
-      let myForm = this.form
-      let content = myForm.querySelector('input[name=content]').value
-      let name = myForm.querySelector('input[name=name]').value
-      this.model.save({
-        'name':name, 'content':content
-      }).then(function(object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.appendChild(li)
-        myForm.querySelector('input[name=content]').value = ''
-        console.log(object)
-      })
-    }
+        init: function (view, model) {
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('form')
+            this.loadMessages()
 
-  })
+        },
+        loadMessages: function () {
+            this.model.fetch().then(
+                (messages) => {
+                    let array = messages.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}: ${item.content}`
+                        this.messageList.appendChild(li)
+                    })
+                }
+            )
+        },
+        bindEvents: function () {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.saveMessage()
+            })
+        },
+        saveMessage: function () {
+            let myForm = this.form
+            let content = myForm.querySelector('input[name=content]').value
+            let name = myForm.querySelector('input[name=name]').value
+            this.model.save({
+                'name': name, 'content': content
+            }).then(function (object) {
+                let li = document.createElement('li')
+                li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+                myForm.querySelector('input[name=content]').value = ''
+                console.log(object)
+            })
+        }
 
-  controller.init(view, model)
+
+    })
+
+
+    controller.init(view, model)
 
 
 }.call()
-
-
-
 
